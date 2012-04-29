4ed67c4c03bac21d792899f911ac97af02651710
Eirik Brandtzæg <eirikb@eirikb.no>
2012-04-29 23:32:10 +0200

    No more setTimeouthack

--- h5z1/objectdefs/monsters/zombie.js
+++ h5z1/objectdefs/monsters/zombie.js
@@ -1,59 +1,54 @@
 (function() {
-    var zombies, way;
+    var zombies;
 
-    // This should work!
-    //events.on('onload', function() {
-    //});
-    setTimeout(function() {
-        zombies = _.filter(game.objects, function(object) {
-            return object.objectdef === 'zombie';
-        });
+    zombies = [];
 
-        _.each(zombies, function(zombie) {});
+    events.on('objectCreate', function(object) {
+        if (object.objectdef === 'zombie') zombies.push(object);
+    });
 
-        events.on('tick', function() {
-            var v;
+    events.on('tick', function() {
+        var v;
 
-            _.each(zombies, function(zombie) {
-                var prevWay, animation;
+        _.each(zombies, function(zombie) {
+            var way, animation;
 
-                prevWay = way;
-                if (trolley.pos(game.player.body).x > trolley.pos(zombie.body).x) way = 1;
-                else way = - 1;
+            if (!zombie.prevWay) zombie.prevWay = 0;
+            if (trolley.pos(game.player.body).x > trolley.pos(zombie.body).x) way = 1;
+            else way = - 1;
 
-                if (prevWay !== way) {
-                    if (way === 1) animation = 'right';
-                    else animation = 'left';
-                    zombie.graphics[0].gotoAndPlay(animation);
-                }
-
-                v = zombie.body.GetLinearVelocity();
-                v.Set(way * zombie.speed, v.y);
-            });
-        });
-
-        events.on('collide', function(a, b) {
-            var zombie, v, damage, power;
-
-            zombie = _.find([a, b], function(o) {
-                return o.objectdef === 'zombie';
-            });
-            if (zombie === a) a = b;
-
-            if (zombie) {
-                v = a.body.GetLinearVelocity();
-                if (a.power) power = a.power;
-                else power = 1;
-
-                damage = Math.floor(((Math.abs(v.x) + Math.abs(v.y)) * a.body.GetMass() / 10) * power);
-                zombie.life -= damage;
-
-                if (zombie.life <= 0) {
-                    game.destroyObject(zombie);
-                }
+            if (zombie.prevWay !== way) {
+                if (way === 1) animation = 'right';
+                else animation = 'left';
+                zombie.graphics[0].gotoAndPlay(animation);
             }
+            zombie.prevWay = way;
+
+            v = zombie.body.GetLinearVelocity();
+            v.Set(way * zombie.speed, v.y);
         });
-    },
-    1000);
+    });
+
+    events.on('collide', function(a, b) {
+        var zombie, v, damage, power;
+
+        zombie = _.find([a, b], function(o) {
+            return o.objectdef === 'zombie';
+        });
+        if (zombie === a) a = b;
+
+        if (zombie) {
+            v = a.body.GetLinearVelocity();
+            if (a.power) power = a.power;
+            else power = 1;
+
+            damage = Math.floor(((Math.abs(v.x) + Math.abs(v.y)) * a.body.GetMass() / 10) * power);
+            zombie.life -= damage;
+
+            if (zombie.life <= 0) {
+                game.destroyObject(zombie);
+            }
+        }
+    });
 })();
 
