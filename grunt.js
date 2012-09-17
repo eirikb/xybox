542b791edc88429084adcbe2da3e093c096e4247
Eirik Brandtzæg <eirikb@eirikb.no>
2012-09-17 21:10:57 +0000

    divided into "dist" and "all"

--- grunt.js
+++ grunt.js
@@ -3,8 +3,13 @@ module.exports = function(grunt) {
     grunt.initConfig({
         concat: {
             dist: {
-                src: ['components/box2d.min.js', 'components/easeljs-0.4.1.min.js', 'components/kibo.js', 'components/preloadjs-0.1.0.min.js', 'components/trolley.js', 'components/underscore.js', 'src/game.js', 'src/graphics.js', 'src/events.js'],
+                src: ['src/helpers.js', 'src/events.js', 'src/preload.js', 'src/physics.js', 'src/graphics.js', 'src/game.js'],
                 dest: 'xybox.js',
+                separator: ';'
+            },
+            all: {
+                src: ['components/box2d.min.js', 'components/easeljs-0.4.1.min.js', 'components/kibo.js', 'components/preloadjs-0.1.0.min.js', 'components/trolley.js', 'components/underscore.js', 'src/helpers.js', 'src/events.js', 'src/preload.js', 'src/physics.js', 'src/graphics.js', 'src/game.js'],
+                dest: 'xybox-all.js',
                 separator: ';'
             }
         },
@@ -12,9 +17,13 @@ module.exports = function(grunt) {
             dist: {
                 src: ['xybox.js'],
                 dest: 'xybox.min.js'
+            },
+            all: {
+                src: ['xybox-all.js'],
+                dest: 'xybox-all.min.js'
             }
         }
     });
 
-    grunt.registerTask('default', 'concat', 'min');
+    grunt.registerTask('default', 'concat min');
 };
