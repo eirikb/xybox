c4f02cb75e1cf70046800863f6c10c1c8044a500
Eirik Brandtzæg <eirikb@eirikb.no>
2012-10-18 19:17:52 +0000

    smaller debuggraphics scale

--- debuggraphics.js
+++ debuggraphics.js
@@ -5,7 +5,7 @@
         var c = document.getElementById('debugpanel'),
         ctx = c.getContext('2d');
         var dbgDraw = new b2DebugDraw();
-        dbgDraw.m_drawScale = 50;
+        dbgDraw.m_drawScale = 10;
         dbgDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit | b2DebugDraw.e_centerOfMassBit);
         dbgDraw.SetSprite(ctx);
         physics.world.SetDebugDraw(dbgDraw);
