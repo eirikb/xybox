30a0d8c39b13930ef99a0b8cf782c7db6e880697
Eirik Brandtzæg <eirikb@eirikb.no>
2012-11-01 18:50:08 +0000

    game.load

--- demos/bomberman/bomberman.js
+++ demos/bomberman/bomberman.js
@@ -5,12 +5,13 @@ bomberman = function(level) {
     bar.style.width = '0%';
     progress.style.display = '';
 
-    game.init('levels/level-' + level, function(count, total) {
+    game.load('levels/level-' + level, function(count, total) {
         var percentage = Math.floor(count / total * 100);
         bar.style.width = percentage + '%';
         if (count === total) progress.style.display = 'none';
     });
 };
+
 window.onload = function() {
     bomberman(1);
 };
