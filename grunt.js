a226ceefb9e6c4c14c10bf23e8b91ad31913d008
eirikb <eirikb@eirikb.no>
2013-06-15 23:32:00 +0200

    Rename grunt.js to Gruntfile.js (new setup)

--- grunt.js
+++ grunt.js
@@ -1,29 +1,29 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        concat: {
-            dist: {
-                src: ['src/helpers.js', 'src/events.js', 'src/preload.js', 'src/physics.js', 'src/graphics.js', 'src/game.js'],
-                dest: 'xybox.js',
-                separator: ';'
-            },
-            all: {
-                src: ['components/box2d/index.js', 'components/easel/index.js', 'components/kibo/index.js', 'components/preload/index.js', 'components/trolley/trolley.js', 'components/underscore/underscore.js', 'src/helpers.js', 'src/events.js', 'src/preload.js', 'src/physics.js', 'src/graphics.js', 'src/game.js'],
-                dest: 'xybox-all.js',
-                separator: ';'
-            }
-        },
-        min: {
-            dist: {
-                src: ['xybox.js'],
-                dest: 'xybox.min.js'
-            },
-            all: {
-                src: ['xybox-all.js'],
-                dest: 'xybox-all.min.js'
-            }
-        }
-    });
+  grunt.initConfig({
+    concat: {
+      dist: {
+        src: ['src/helpers.js', 'src/events.js', 'src/preload.js', 'src/physics.js', 'src/graphics.js', 'src/game.js'],
+        dest: 'xybox.js',
+        separator: ';'
+      },
+      all: {
+        src: ['components/box2d/index.js', 'components/easel/index.js', 'components/kibo/index.js', 'components/preload/index.js', 'components/trolley/trolley.js', 'components/underscore/underscore.js', 'src/helpers.js', 'src/events.js', 'src/preload.js', 'src/physics.js', 'src/graphics.js', 'src/game.js'],
+        dest: 'xybox-all.js',
+        separator: ';'
+      }
+    },
+    min: {
+      dist: {
+        src: ['xybox.js'],
+        dest: 'xybox.min.js'
+      },
+      all: {
+        src: ['xybox-all.js'],
+        dest: 'xybox-all.min.js'
+      }
+    }
+  });
 
-    grunt.registerTask('default', 'concat min');
+  grunt.registerTask('default', 'concat min');
 };
