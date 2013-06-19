d245eeb0adf3ee583317af2727ac554845c65c00
eirikb <eirikb@eirikb.no>
2013-06-19 21:49:39 +0200

    Example with some imaginary physics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 $(function() {
   prettyPrint();
 
-  for (var i = 1; i <= 6; i++) {
+  for (var i = 1; i <= 7; i++) {
     var game = new xybox.Game();
     game.init('ball' + i, 'defs/ball' + i + '.json');
-    new Debugpanel('ball6-debug', game);
   }
 });
