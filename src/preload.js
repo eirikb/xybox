40ec58c8ac484fbffb61edc08cdb6007c224897d
eirikb <eirikb@eirikb.no>
2013-06-20 14:37:02 +0200

    Code support when preloading (script files)

--- src/preload.js
+++ src/preload.js
@@ -3,20 +3,21 @@ function Preload(game, center) {
   var self = this;
   var allDefs;
 
+  var src;
+
+  function addPath(def) {
+    var path = src.match(/.*\//);
+    path = path ? path[0] : '';
+    path += def;
+    if (!path.match(/\.js(on)?$/i)) path += '.json';
+    return path;
+  }
+
   function loadDefs(defPaths, cb) {
     defPaths = _.flatten([defPaths]);
 
     var queue = new createjs.LoadQueue();
     var defs = [];
-    var src;
-
-    function addPathToDef(def) {
-      var path = src.match(/.*\//);
-      path = path ? path[0] : '';
-      path += def;
-      if (!path.match(/\.json$/i)) path += '.json';
-      return path;
-    }
 
     queue.addEventListener('fileload', function(e) {
       var result = e.result;
@@ -38,7 +39,7 @@ function Preload(game, center) {
 
       _.each(defs, function(def) {
         if (def.includes) {
-          def.includes = _.map(def.includes, addPathToDef);
+          def.includes = _.map(def.includes, addPath);
           loadDefs(def.includes, cb);
         } else {
           count--;
@@ -59,11 +60,16 @@ function Preload(game, center) {
 
     queue.addEventListener('fileload', function(e) {
       center.assets[e.item.id] = e.result;
+      if (e.item.ext === 'js') document.body.appendChild(e.result);
     });
 
     queue.addEventListener('complete', cb);
 
     _.each(defsAndItems, function(defOrItem, name) {
+      var code = defOrItem.code;
+      if (code) code = code.match(/(.*) /);
+      if (code) manifest.push(addPath(code[1]));
+
       _.each(defOrItem.graphics, function(graphic) {
         var img = graphic.image;
         if (img) manifest.push(img);
