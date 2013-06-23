444f9b9bb786bfa2102e9c095766fd356ec20255
eirikb <eirikb@eirikb.no>
2013-06-23 23:47:33 +0200

    Map stuff

--- defs/tutorial/thebomb.js
+++ defs/tutorial/thebomb.js
@@ -1,23 +1,35 @@
 theBomb = function(game) {
 
-  function rand(wh) {
-    return Math.floor(Math.random() * ((wh / game.scale) - 1));
+  function rand(x) {
+    return Math.floor(Math.random() * x);
+  }
+
+  function scale(x) {
+    return Math.floor((x / game.scale) - 1);
+  }
+
+  function createBomb(x, y) {
+    game.createItem({
+      def: 'bomb',
+      power: rand(7) + 3,
+      body: {
+        x: scale(x),
+        y: scale(y)
+      }
+    });
   }
 
   setInterval(function() {
-    game.createItem({
-      def: 'bomb',
-      power: Math.floor(Math.random() * 7) + 3,
-      body: {
-        x: rand(game.width),
-        y: rand(game.height)
-      }
-    });
+    createBomb(rand(game.width), rand(game.height));
   }, 1000);
 
   game.on('tick', function() {
     _.each(game.itemByDef.bomb, function(bomb) {
       if (--bomb.timer <= 0) game.destroyItem(bomb);
     });
+  });
+
+  game.on('mouseDown', function(e) {
+    createBomb(e.stageX, game.height - e.stageY);
   });
 };
