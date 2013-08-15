ffee226f85df0ae7d5fcf5d583b012919ecb4fca
eirikb <eirikb@eirikb.no>
2013-08-15 23:41:38 +0200

    Specific grunt plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,12 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib');
+  //grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask('default', ['concat', 'jshint', 'uglify', 'copy']);
 
